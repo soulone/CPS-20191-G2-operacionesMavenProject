aede426aea39d1dcd04c2757bce4910f5ff5a921
soulone <ascf24032@gmail.com>
2019-05-10 16:07:35 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab11/operacionesMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab11.operacionesMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
